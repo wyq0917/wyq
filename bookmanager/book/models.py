eafd04144b513bc589b73a905280e83abea497a0
ubuntu <you@example.com>
2020-08-16 16:32:32 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+'''
+书籍表
+id	name
+1	西游记
+2	三国演义
+人物表
+id	name	gender	book
+1	孙悟空	False	1
+2	白骨精	True	1
+3	曹操	    False	2
+4	貂蝉	    True	2
+'''
+class BookInfo(models.Model):
+    # 创建字段，字段类型...
+    name = models.CharField(max_length=10)
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    # 外键约束：人物属于哪本书
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
