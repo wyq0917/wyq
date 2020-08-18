a549b5661630b912601bec4efd0392ff9fffcd9d
ubuntu <you@example.com>
2020-08-18 21:30:37 +0800

    模型的定义

--- bookmanageer02/book01/models.py
+++ bookmanageer02/book01/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    # 创建字段，字段类型...
+    name = models.CharField(max_length=20, verbose_name='名称')
+    pub_date = models.DateField(verbose_name='发布日期',null=True)
+    readcount = models.IntegerField(default=0, verbose_name='阅读量')
+    commentcount = models.IntegerField(default=0, verbose_name='评论量')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+# class Meta:
+#     db_table = 'bookinfo'  # 指明数据库表名
+#     verbose_name = '图书'
